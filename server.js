4fb9cfb2eeb3f3d68b311af63657d43a3d36255c
yoslandy010316 <yoslandy010316@gmail.com>
2020-05-15 07:53:12 -0400

    fixed first sscommit

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ app.use(express.json());
 /* var db = 'mongodb+srv://user:user@shoppinglist-3cfnv.mongodb.net/test?retryWrites=true&w=majority'; */
 var db = config.get('mongoURI');
 
-
 //Connect Mongodb
 /* mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true); */
